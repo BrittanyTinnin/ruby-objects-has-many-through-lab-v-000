d742d3086557d7b88d6a4d2d3cba47fb4937e6bf
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-09 12:19:44 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,4 +15,8 @@ class Patient
   def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
+
+  def appointments
+    Appointment.all.select{|appt| appt.patient = self}
+  end
 end
