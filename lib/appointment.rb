7dc91917e2088385158b15e2c0d22c42cb0b6b23
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-09 12:08:26 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -7,6 +7,10 @@ class Appointment
 
   attr_accessor :patient, :doctor, :date
 
-
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+  end
 
 end
