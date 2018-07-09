6b875ca0a19c60b3806f4e81b1eeeed013b8342b
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-09 11:38:24 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+
+  end
+
+end
